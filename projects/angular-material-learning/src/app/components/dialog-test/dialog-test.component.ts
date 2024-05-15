bad04f31b5a070e677190a49cd1e2de0473225ce
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-15 22:51:22 +0530

    Dialog Basics

---
+++ projects/angular-material-learning/src/app/components/dialog-test/dialog-test.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-dialog-test',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './dialog-test.component.html',
+  styleUrl: './dialog-test.component.css'
+})
+export class DialogTestComponent {
+
+  /**
+   *
+   */
+  constructor(private dialog: MatDialog) {
+    
+  }
+
+  openDialog(): void {
+    this.dialog.open(DialogComponent, {
+      width: '50%',
+      height: '40%'
+
+    });
+  }
+
+}
